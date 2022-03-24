23d309174ab36ef53e35c363ccd373e9ed9f2545
Kevin051596 <yellow0515car@gmail.com.tw>
2022-03-24 16:22:19 +0800

    website update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,23 @@
 <template>
 
   <header :class="{'scrolled-nav': scrollNav}">
-        <ul>
+        <ul v-show="!mobile">
             <li><router-link :to="{name:'Home', hash:'#time'}">首頁</router-link> </li>
             <li><router-link :to="{name:'Photos', hash:'#photos'}">相簿</router-link> </li>
             <li><router-link :to="{name:'Comment', hash:'#comment'}">評價</router-link> </li>
             <li><router-link to="/#contact">聯絡我們</router-link> </li>
         </ul>
+        <div class="navbar_icon">
+          <i @click="toggleMobileNav" v-show="mobile" class="fi fi-br-menu-burger" :class="{'icon-active':mobileNav}" ></i>
+        </div>
+        <transition name='mobile-nav'>
+            <ul v-show="mobileNav" class="dropdown-nav">
+              <li><router-link :to="{name:'Home', hash:'#time'}">首頁</router-link> </li>
+              <li><router-link :to="{name:'Photos', hash:'#photos'}">相簿</router-link> </li>
+              <li><router-link :to="{name:'Comment', hash:'#comment'}">評價</router-link> </li>
+              <li><router-link to="/#contact">聯絡我們</router-link> </li>
+            </ul>
+        </transition>
   </header>
 
 </template>
@@ -18,13 +29,21 @@ export default {
     return{
       mobileNav: null,
       scrollNav: null,
+      windowWidth: null,
+      mobile:null,
       };
   },
-
+  created(){
+      window.addEventListener('resize', this.checkScreen);
+      this.checkScreen();
+  },
   mounted(){
     window.addEventListener('scroll',this.updateScroll);
   },
   methods:{
+    toggleMobileNav(){
+      this.mobileNav = !this.mobileNav;
+    },
     updateScroll(){
       const scrollPosition = window.scrollY;
       if(scrollPosition > 20){
@@ -33,6 +52,16 @@ export default {
       }
       this.scrollNav = false;
     },
+    checkScreen(){
+        this.windowWidth = window.innerWidth;
+        if(this.windowWidth <= 850){
+            this.mobile = true;
+            return
+        }
+        this.mobile = false;
+        this.mobileNav = false;
+        return;
+    },
   }
 };
 </script>
@@ -40,4 +69,5 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "@/assets/scss/navbar.scss";
+@import url('https://cdn-uicons.flaticon.com/uicons-bold-rounded/css/uicons-bold-rounded.css');
 </style>
