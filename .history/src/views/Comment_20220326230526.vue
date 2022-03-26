8b6013769f60c5dd4bc30208b52a6860a2d11e4d
Kevin051596 <yellow0515car@gmail.com.tw>
2022-03-26 23:25:47 +0800

    swipe update

---
+++ .history/src/views/Comment_20220326230526.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="comment">
+    <div class="content-titlewrapper">
+        <i class="decoration"></i>
+        <p class="content-title">相關評論</p>
+        <i class="decoration"></i>
+    </div>
+        <img 
+    </section> 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/Home/comment.scss";
+</style>
---
+++ .history/src/views/Photos_test_20220326221015.vue
@@ -0,0 +1,104 @@
+<template>
+  <section id="photos">
+        <div class="content-titlewrapper">
+          <i class="decoration"></i>
+          <p class="content-title">相關照片</p>
+          <i class="decoration"></i>
+        </div>
+      <!-- <i class="btn">更多照片</i> -->
+          <div class="wrapper" v-show="!mobile">
+              <div class="gallery">
+                  <div class="image" v-for="(img, index) in images" :key="index">
+                    <span><img :src="require(`@/assets/picture/${img}.jpg`)" alt=""></span>
+                </div>
+              </div>
+          </div>
+          <div class="preview-box" v-show="!mobile">
+            <div class="details">
+                <span class="title">Image <p class="current-img"></p> of <p class="total-img"></p></span>
+                <span class="icon fas fa-times"></span>
+            </div>
+            <div class="image-box" v-show="!mobile">
+                <div class="slide prev"><i class="fas fa-angle-left"></i></div>
+                <div class="slide next"><i class="fas fa-angle-right"></i></div>
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="shadow"></div>
+        <div class="swiper-area">
+            <Swiper 
+                v-show="mobile"
+                :slides-per-view="1"
+                :space-between="50"
+                :modules="modules"
+                navigation
+                :pagination="{ clickable: true, dynamicBullets:true }"
+                grab-cursor
+                virtual
+                @swiper="onSwiper"
+                @slideChange="onSlideChange"
+            >
+                <SwiperSlide v-for="(img, index) in images" :key="index" :virtualIndex="index">
+                    <img :src="require(`@/assets/picture/${img}.jpg`)"/>
+                    
+                </SwiperSlide>
+
+            </Swiper>
+        </div>
+
+    </section>
+</template>
+
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Navigation, Pagination,  Virtual } from 'swiper'
+import 'swiper/swiper-bundle.css'
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        const onSwiper = (swiper) => {
+        console.log(swiper);
+      };
+      const onSlideChange = () => {
+        console.log('slide change');
+      };
+        return {
+            modules: [Navigation, Pagination, Virtual],
+        };
+    },
+
+
+    data(){
+
+        return{
+            mobile:null,
+            images:['001','002','003','004','005','006','007'],
+        }
+    },
+
+    created(){
+      window.addEventListener('resize', this.checkScreen);
+      this.checkScreen();
+    },
+    methods:{
+        checkScreen(){
+            this.windowWidth = window.innerWidth;
+            if(this.windowWidth <= 850){
+                this.mobile = true;
+                return
+                }
+            this.mobile = false;
+            return;
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/Home/gallery.scss";
+</style>
